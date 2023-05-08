cf814c88138965ddcefe0b973d248a1cf5f0e911
ORUCALIYEV <orucaliyefff@gmail.com>
2023-05-08 05:13:24 +0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p>
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
+};
+
+export default Home;
